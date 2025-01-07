60948a35f414ad9c024511886ef40a4c85eda2bc
Giancarlos1024 <giancarlosvlsqz@gmail.com>
2025-01-07 07:21:46 -0500

    se actualizo el footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,23 +8,23 @@ export const Footer = () => {
       <section>
         <div className='informacion-footer'>
           <div className='unidad-educativa'>
-            <h3>UNIDAD EDUCATIVA MANUEL INOCENCIO PARRALES Y GUALE</h3>
-            <p>Unidad Educativa Fiscal © - Eficiencia y Disciplina Acuerdo Ministerial ® : N° 016858, 26 de Agosto de 1966™.</p>
+            <h3>UNIDAD EDUCATIVA REPUBLICA D EL ECUADOR</h3>
+            {/* <p>Unidad Educativa Fiscal © - Eficiencia y Disciplina Acuerdo Ministerial ® : N° 016858, 26 de Agosto de 1966™.</p> */}
           </div>
           <div className='contacto'>
             <h3>PONTE EN CONTACTO</h3>
             <p>DIRECCION</p><br />
-            <p>Av. Luis Bustamante, Jipijapa, Ecuador</p>
+            <p>KM 1 vía al Rcto Chade, Jipijapa, Ecuador</p>
           </div>
           <div className='telefono'>
             <h3><img src="img/phone.png" alt="telefono" />TELEFONO</h3>
-            <p>593-5 2654 383.</p>
+            {/* <p>593-5 2654 383.</p> */}
           </div>
         </div>
         <div className='redes_sociales'>
           <a href="#"><img src="img/twitter_red.png" alt="twitter" /></a>
           <a href="#"><img src="img/linkedin_red.png" alt="linkedin" /></a>
-          <a href="#"><img src="img/facebook_red.png" alt="facebook" /></a>
+          <a href="https://web.facebook.com/amre.jipijapa?mibextid=LQQJ4d&rdid=qTFaabUNt0FG3kVO&share_url=https%3A%2F%2Fweb.facebook.com%2Fshare%2F14zodHC3KR%2F%3Fmibextid%3DLQQJ4d%26_rdc%3D1%26_rdr" target='_blank'><img src="img/facebook_red.png" alt="facebook" /></a>
           <a href="#"><img src="img/instagram_red.png" alt="instagram" /></a>
         </div>
       
